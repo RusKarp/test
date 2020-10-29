17322144f4137ecdfb8b9ad3b7173781afa8870a
Ruslan <rus.karp88@mail.ru>
2020-10-29 13:20:59 +0200

    Создал форму ввода

--- views/index.php
+++ views/index.php
@@ -23,6 +23,48 @@ include_once '../app/bootstrap.php';
     </style>
 </head>
 <body>
+<!--Форма для ввода даных-->
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="form-signin">
+                <form class="form-signin" action="../app/add.php" method="post">
+
+
+                    <input class="form-control" placeholder="Имя" type="text" name="name"
+                           value=""/><br/><br/><!--&nbsp;-->
+
+                    <input class="form-control" placeholder="Фамилия" type="text" name="last_name"
+                           value=""/><br/><br/>
+
+                    <input class="form-control" type="text" placeholder="Номер телефона" minlength="13" maxlength="13" name="phone"
+                           value=""/><br/><br/>
+
+                    <input class="form-control" type="text" name="age" placeholder="Возрост"
+                           value=""/><br/><br/>
+
+                    <input class="form-control" type="text" name="gender" placeholder="Вкажите пол"
+                           value=""/><br/><br/>
+
+                    <input class="form-control" type="text" name="group" placeholder="Група"
+                           value=""/><br/><br/>
+
+                    <input class="form-control" type="text" name="faculty" placeholder="Факультет"
+                           value=""/><br/><br/>
+
+
+                    <input  type="hidden" name="token" value="<?= session_id() ?>"/>
+
+                    <input type="submit" class="btn btn-lg btn-primary btn-block" name="submit_add" value="Добавить"/>
+
+
+                </form>
+
+            </div>
+        </div>
+    </div>
+</div>
+<br/>
 
 
 
