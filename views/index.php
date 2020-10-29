564537753a79ce0fb27cc0f07037cbbca3fd7f01
Ruslan <rus.karp88@mail.ru>
2020-10-29 09:49:17 +0200

    Подключение файла bootstrap.php

--- views/index.php
+++ views/index.php
@@ -1,37 +1,28 @@
+<?php
+//подключяймы файл
+include_once '../app/bootstrap.php';
+
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Template</title>
-    <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
+    <title>Test</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
           integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
--->
-<style type="text/css">
-   
-   
-  </style>
+
+    <style type="text/css">
+
+
+    </style>
 </head>
 <body>
-<?php
-include_once
-
-?>
-
-
-
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-
-Test
-
-            </div>
-        </div>
-    </div>
-</div>
 
 
 
@@ -40,16 +31,6 @@ Test
 
 </body>
 </html>
-
-
-
-
-
-
-
-
-
-
 
 
 
